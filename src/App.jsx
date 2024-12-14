24ad8243a32591aaa6065d98878159d1e059527a
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-14 14:32:40 +0530

    ti commit

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ const App = () => {
     const fetchData = async () => {
       try {
         // const response = await fetch("http://127.0.0.1:8000/robots");
-        const response = await fetch("https://robot-backend-1.onrender.com");
+        const response = await fetch("https://robot-backend-1.onrender.com/robots");
         const data = await response.json();
         setRobots(data);
         setLastUpdated(new Date());
